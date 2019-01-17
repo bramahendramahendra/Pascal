62d327806257df5cfe22d67148052a42ae725535
bramahendramahendra <bramahendramahendra@gmail.com>
2019-01-17 08:18:58 +0700

    Pascal
    Pascal

---
+++ 00000 Input dan Output Integer/baca1.pas
@@ -0,0 +1,12 @@
+program baca1;
+Uses crt;
+var
+        a : integer;
+
+begin
+        clrscr;
+        writeln('contoh memmbaca dan menulis, ketik nilai integer');
+        readln(a);
+        writeln('nilai yang dibaca : ',a);
+        readln;
+end.
